9d108373b69b0918a2ebac20d8e9f6286000f626
Ashik-AD <looge321@gmail.com>
2022-08-29 15:40:41 +0545

    IDK what i did

--- src/ui/buttons/Button.tsx
+++ src/ui/buttons/Button.tsx
@@ -2,12 +2,22 @@ import { FC } from "react";
 import NormalButton from "./Button.style";
 import { ButtonType } from "./type";
 import useTheme from "../../theme/useTheme";
-const Button: FC<ButtonType> = ({ varient = "text", children, color }) => {
+const Button: FC<ButtonType> = ({
+  varient = "text",
+  children,
+  color,
+  onClick,
+}) => {
   const {
     theme: { primaryColor },
   } = useTheme();
   return (
-    <NormalButton theme={{ primaryColor }} varient={varient} color={color}>
+    <NormalButton
+      theme={{ primaryColor }}
+      varient={varient}
+      color={color}
+      onClick={onClick}
+    >
       {children}
     </NormalButton>
   );
--- src/ui/buttons/type/index.ts
+++ src/ui/buttons/type/index.ts
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { PrimaryColor } from "../../../theme/colors";
 
 type Varient = 'text' | 'contained' |  'outlined';
@@ -6,10 +7,11 @@ type Size = 'small' | 'medium' | 'large';
 
 export interface ButtonType {
   readonly  varient?: Varient; 
-   readonly children: string;
+   readonly children: string | ReactNode;
    readonly color?: Color;
    readonly size?: Size;
    readonly theme?: {
     primaryColor: PrimaryColor;
    }
+   readonly onClick?: () => void;
 }   
