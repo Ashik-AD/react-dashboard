1788dd15e286b506b23c44a2e4b1c3a8a22db879
Ashik-AD <looge321@gmail.com>
2022-08-29 15:41:23 +0545

    add default export for uis

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,1 +1,7 @@
-export {default as Text} from './text/Text';
+export { default as Text } from './text/Text';
+export { default as Button } from './buttons/Button';
+export { default as Checkbox } from './checkbox/Checkbox';
+export { default as ChoiceGroup } from './choice/ChoiceGroup';
+export { default as Choice } from './choice/Choice';
+export { default as Avtar } from './avatar/Avatar';
+export { default as CustomAvatar } from './avatar/CustomAvatar';
