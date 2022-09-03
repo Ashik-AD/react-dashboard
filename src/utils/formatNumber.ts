06730049cb4eaa263bb0c1425fafa37f1d861b0a
Ashik-AD <looge321@gmail.com>
2022-09-03 14:32:00 +0545

    Add option args 🤔🤔

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -1,21 +1,21 @@
-export default function formatNumber(value: number, divider?: number): string {
+export default function formatNumber(value: number, divider?: number, option?: { toFixed: number }): string {
     if (!divider || value < divider!) {
         return addComma(value);
     }
     else {
-        return `${Math.abs(value / divider).toFixed(2)}${units(divider.toString().length)}`;
+        return `${Math.abs(value / divider).toFixed(option?.toFixed ? option.toFixed : 2)}${units(divider.toString().length)}`;
     }
 }
 
 function units(length: number) {
     if (length >= 4 && length <= 5) {
-        return 'K';
+        return 'k';
     }
     else if (length >= 6 && length <= 8) {
-        return 'M';
+        return 'm';
     }
     else if (length >= 9 && length <= 10) {
-        return 'B';
+        return 'b';
     }
     return;
 }
