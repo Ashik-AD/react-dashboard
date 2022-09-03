8ec96791ff6d135b90ccaffedb54e37a41f36876
Ashik-AD <looge321@gmail.com>
2022-09-03 18:27:48 +0545

    Add transaction card 🛍️💸

---
+++ src/components/card/statistics/CardTransaction.tsx
@@ -0,0 +1,56 @@
+import { FC, ReactNode } from "react";
+import { CustomAvatar, Text } from "../../../ui";
+import formatNumber from "../../../utils/formatNumber";
+import Box from "../../box/Box";
+import { GridContainer, GridItem } from "../../layout";
+import Card from "../Card";
+import CardTitle from "../CardTitle";
+
+const CardTransaction: FC<PropsType> = (props) => {
+  const { title, items } = props;
+  return (
+    <Card>
+      <CardTitle title={title} />
+      <Box padding={20}>
+        <GridContainer>
+          {items.map((item) => (
+            <GridItem key={item.id} lg={3} md={3} xs={6}>
+              <Box display="flex" align="center" space={1} pt={5}>
+                <CustomAvatar color={item.color} varient="rounded">
+                  {item.icon}
+                </CustomAvatar>
+                <Box display="flex" flexDirection="column" space={0.3}>
+                  <Text
+                    varient="caption"
+                    styles={{ textTransform: "capitalize", fontWeight: 500 }}
+                  >
+                    {item.title}
+                  </Text>
+                  <Text heading="h6">
+                    {item.currency && "$"}
+                    {formatNumber(item.total, 1000)}
+                  </Text>
+                </Box>
+              </Box>
+            </GridItem>
+          ))}
+        </GridContainer>
+      </Box>
+    </Card>
+  );
+};
+
+interface PropsType {
+  title: string;
+  children?: ReactNode;
+  items: {
+    id: number | string;
+    icon: ReactNode;
+    title: string;
+    total: number;
+    currency?: boolean;
+    color?: string;
+  }[];
+}
+
+export default CardTransaction;
