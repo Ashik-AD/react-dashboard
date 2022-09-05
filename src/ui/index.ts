0494f7a1225bbe2aa79fd513b784296792c3355e
Ashik-AD <looge321@gmail.com>
2022-09-05 15:31:35 +0545

    Fix typo default avatar export name 😁😌😌

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -4,7 +4,7 @@ export { default as IconButton } from './buttons/IconButton';
 export { default as Checkbox } from './checkbox/Checkbox';
 export { default as ChoiceGroup } from './choice/ChoiceGroup';
 export { default as Choice } from './choice/Choice';
-export { default as Avtar } from './avatar/Avatar';
+export { default as Avatar } from './avatar/Avatar';
 export { default as CustomAvatar } from './avatar/CustomAvatar';
 export { default as Toggle } from './toggle/Toggle';
 export { default as Divider } from './divider/Divider';
