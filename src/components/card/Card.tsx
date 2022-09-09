8bd0b3551b66789007c627849e200f537b0fa00c
Ashik-AD <looge321@gmail.com>
2022-09-09 10:08:08 +0545

    Add class name props 💃🕺

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -6,7 +6,7 @@ import { CardWrapper } from "./card.styled";
 const Card: FC<CardProps> = (props) => {
   const { theme } = useTheme();
   return (
-    <CardWrapper theme={theme} {...props}>
+    <CardWrapper theme={theme} {...props} className={props.className}>
       {props.children}
     </CardWrapper>
   );
