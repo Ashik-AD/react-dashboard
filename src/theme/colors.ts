0891cac25d31c4e0ad04500fd76b9aa6881fa60e
Ashik-AD <looge321@gmail.com>
2022-09-13 22:37:30 +0545

    add more colors 🥳🌈🪅🪄

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -17,6 +17,15 @@ export type blue = {
     color: '#00D7FF'
 }
 
+export type orange = {
+    name: 'orange',
+    color: '#FF5B00'
+}
+export type pinky = {
+    name: 'pinky',
+    color: '#F94892'
+}
+
 export interface Dark {
     name: 'dark';
     background: '#1B2430';
@@ -31,10 +40,12 @@ export interface Light {
 }
 
 export type ThemeMode = Dark | Light;
-export type PrimaryColor = red | green | purple | blue;
+export type PrimaryColor = red | green | purple | blue | orange | pinky;
 export const colorPallets: PrimaryColor[] = [
     { name: 'red', color: "#FD5D5D" },
     { name: 'green', color: "#3CCF4E" },
     { name: 'blue', color: "#00D7FF" },
-    { name: 'purple', color: "#A66CFF" }
+    { name: 'purple', color: "#A66CFF" },
+    { name: 'orange', color: "#FF5B00" },
+    { name: 'pinky', color: "#F94892" },
 ];
