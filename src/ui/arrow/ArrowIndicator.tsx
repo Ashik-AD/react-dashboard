416e003216ef2b7cf8fe04b045fc4d77008e7d30
Ashik-AD <looge321@gmail.com>
2022-09-19 22:02:54 +0545

    add arrow indicator 👆👇

---
+++ src/ui/arrow/ArrowIndicator.tsx
@@ -0,0 +1,40 @@
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { CSSProperties, FC, ReactNode } from "react";
+
+const ArrowIndicator: FC<Props> = (props) => {
+  const { arrowFirst, arrowSecond, status, color, style } = props;
+  return (
+    <span
+      className={
+        color
+          ? status === "inc"
+            ? "alert-success-text"
+            : "alert-error-text"
+          : ""
+      }
+      style={style}
+    >
+      {arrowFirst || arrowSecond ? (
+        status === "inc" ? (
+          arrowFirst
+        ) : (
+          arrowSecond
+        )
+      ) : status === "inc" ? (
+        <ExpandLess />
+      ) : (
+        <ExpandMore />
+      )}
+    </span>
+  );
+};
+
+interface Props {
+  arrowFirst?: ReactNode;
+  arrowSecond?: ReactNode;
+  status: "inc" | "dec";
+  color?: boolean;
+  style?: CSSProperties;
+}
+
+export default ArrowIndicator;
