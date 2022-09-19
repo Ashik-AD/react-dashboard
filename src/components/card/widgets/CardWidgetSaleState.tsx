16d9e0727f8db55935569621b0b89145cf1f4a6a
Ashik-AD <looge321@gmail.com>
2022-09-19 21:53:09 +0545

    remove default chart and added statistic chart

--- src/components/card/widgets/CardWidgetSaleState.tsx
+++ src/components/card/widgets/CardWidgetSaleState.tsx
@@ -1,69 +1,12 @@
-import { ApexOptions } from "apexcharts";
-import ReactApexChart from "react-apexcharts";
 import { Text } from "../../../ui";
 import Box from "../../box/Box";
 import Card from "../Card";
-import CardTitle from "../titles/CardTitle";
-import useTheme from "../../../theme/useTheme";
 import CustomCardTitle from "../titles/CustomCardTitle";
+import StatisticAreaChart from "../../charts/apex/statistics/StatisticAreaChart";
+
+const series: ApexAxisChartSeries = [{ data: [83, 122, 165, 140, 190] }];
+
 const CardWidgetSaleState = () => {
-  const { theme } = useTheme();
-  const chartOptions: ApexOptions = {
-    chart: {
-      type: "area",
-      height: "100%",
-      width: "100%",
-      toolbar: { show: false },
-    },
-    stroke: { colors: [theme.primaryColor.color] },
-    fill: {
-      type: "gradient",
-      colors: [theme.primaryColor.color],
-      gradient: {
-        shadeIntensity: 1,
-        opacityTo: 0,
-        opacityFrom: 0.8,
-        stops: [20, 100],
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    grid: {
-      show: false,
-    },
-    xaxis: {
-      axisBorder: {
-        show: false,
-      },
-      axisTicks: {
-        show: false,
-      },
-      labels: { show: false },
-    },
-    yaxis: {
-      show: false,
-    },
-    tooltip: {
-      enabled: false,
-    },
-    markers: {
-      strokeWidth: 5,
-      offsetY: -6,
-      offsetX: -5,
-      discrete: [
-        {
-          seriesIndex: 0,
-          dataPointIndex: 4,
-          fillColor: "transparent",
-          strokeColor: theme.primaryColor.color,
-          size: 8,
-          shape: "circle",
-        },
-      ],
-    },
-  };
-  const series: ApexAxisChartSeries = [{ data: [83, 122, 165, 140, 190] }];
   return (
     <Card>
       <CustomCardTitle
@@ -78,13 +21,7 @@ const CardWidgetSaleState = () => {
         <Text varient="caption"> </Text>
       </Box>
       <Box>
-        <ReactApexChart
-          type="area"
-          width={"100%"}
-          height={300}
-          options={chartOptions}
-          series={series}
-        />
+        <StatisticAreaChart data={series} height={315} />
       </Box>
     </Card>
   );
