d46f3dd8e0868e6ed034a9b4d91316d9d6340fed
Ashik-AD <looge321@gmail.com>
2022-09-20 11:38:13 +0545

    fix secondary color 😇😇

--- src/ui/text/text.styled.ts
+++ src/ui/text/text.styled.ts
@@ -6,7 +6,7 @@ export const Paragraph = styled('p') <TextType>`
     font-size: ${({ size }) => size && `${size}px`};
     text-align: ${({ align }) => align};
     color: ${({ color, theme }) => color ? alertColor.hasOwnProperty(color) ? alertColor[color as keyof Colors] : color : theme.mode.textColor};
-    color: ${({ secondary, theme }) => secondary && (theme.mode.name === 'dark' ? 'rgb(211 211 211 / 68%);' : 'rgb(25 25 25 / 72%);')}
+    ${({ secondary, theme }) => secondary && `color: ${secondary && (theme.mode.name === 'dark' ? 'rgb(211 211 211 / 68%);' : 'rgb(25 25 25 / 72%);')}`}
     letter-spacing: 0.15px;
     font-weight: ${({ weight }) => weight && fontWeight[weight]};
 `;
@@ -15,8 +15,7 @@ export const TextSpan = styled('span') <TextType>`
     font-size: ${({ size }) => size && `${size}px`};
     text-align: ${({ align }) => align};
     color: ${({ color, theme }) => color ? alertColor.hasOwnProperty(color) ? alertColor[color as keyof Colors] : color : theme.mode.textColor};
-    color: ${({ secondary, theme }) => secondary && (theme.mode.name === 'dark' ? 'rgb(211 211 211 / 68%);' : 'rgb(25 25 25 / 72%);')};
-
+   ${({ secondary, theme }) => secondary && `color: ${secondary && (theme.mode.name === 'dark' ? 'rgb(211 211 211 / 68%);' : 'rgb(25 25 25 / 72%);')}`}
     letter-spacing: 0.15px;
     font-weight: ${({ weight }) => weight && fontWeight[weight]};
 `;
