940b7a097e6d6033c62ad64c1e283cd2098b103d
Ashik-AD <looge321@gmail.com>
2022-09-20 21:09:02 +0545

    add default export 👒🥳🥸🤩

---
+++ src/components/card/widgets/index.ts
@@ -0,0 +1,23 @@
+export { default as CardWidgetProfit } from './CardWidgetProfit'
+
+export { default as CardWidgetTotalRevenue } from './CardWidgetTotalRevenue';
+
+export { default as CardWidgetTotalVisitor } from './CardWidgetTotalVisitor';
+
+export { default as CardWidgetWeeklyOverview } from './CardWidgetWeeklyOverview';
+
+export { default as CardWidgetSaleState } from './CardWidgetSaleState';
+
+export { default as CardWidgetWeeklySales } from './CardWidgetWeeklySales';
+
+export { default as CardWidgetPerformance } from './CardWidgetPerformance';
+
+export { default as CardWidgetAnalytics } from './CardWidgetAnalytics';
+
+export { default as CardWidgetTotalProfitMinimal } from './CardWidgetTotalProfitMinimal';
+export { default as CardWidgetTotalSale } from './CardWidgetTotalSale';
+export { default as CardWidgetTotalVisits } from './CardWidgetTotalVisits';
+
+export { default as CardWidgetRevenueReport } from './CardWidgetRevenueReport';
+
+export { default as CardWidgetSalesOverview } from './CardWidgetSalesOverview';
--- src/components/timeline/index.ts
+++ src/components/timeline/index.ts
@@ -1,4 +1,4 @@
 export { default as Timeline } from './TimeLine';
 export { default as TimelineItem } from './components/TimelineItem';
-export { default as TinelineItemContent } from './components/TimelineItemContent';
+export { default as TimelineItemContent } from './components/TimelineItemContent';
 export { default as TimelineSeparator } from './components/TimelineSeparator'
