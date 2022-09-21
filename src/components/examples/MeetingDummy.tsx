5f8211544bff612a007c7772f2548a5ec5cb0502
Ashik-AD <looge321@gmail.com>
2022-09-21 22:04:52 +0545

    add dummy meeting card

--- src/components/examples/MeetingDummy.tsx
+++ src/components/examples/MeetingDummy.tsx
@@ -17,14 +17,66 @@ const meetings = [
   {
     profile:
       "https://demos.themeselection.com/marketplace/materio-mui-react-nextjs-admin-template/demo-4/images/avatars/2.png",
-    tag: "Dinner",
+    tag: "tech",
     date: {
       month: "Sep",
       day: 6,
       from: "12:00",
       to: "15:30",
     },
-    title: "Dinner with my gf",
+    title: "Nextjs Conf",
+  },
+  {
+    profile:
+      "https://demos.themeselection.com/marketplace/materio-mui-react-nextjs-admin-template/demo-4/images/avatars/8.png",
+    tag: "sport",
+    date: {
+      month: "Sep",
+      day: 21,
+      from: "1:00",
+      to: "15:30",
+    },
+    title: "Launch with Executive",
+    color: "info",
+  },
+  {
+    profile:
+      "https://demos.themeselection.com/marketplace/materio-mui-react-nextjs-admin-template/demo-4/images/avatars/7.png",
+    tag: "travel",
+    date: {
+      month: "Sep",
+      day: 6,
+      from: "12:00",
+      to: "15:30",
+    },
+    title: "Marketing meet",
+    color: "warning",
+  },
+  {
+    profile:
+      "https://demos.themeselection.com/marketplace/materio-mui-react-nextjs-admin-template/demo-4/images/avatars/1.png",
+    tag: "friend",
+    date: {
+      month: "Sep",
+      day: 6,
+      from: "12:00",
+      to: "15:30",
+    },
+    title: "Climate change",
+    color: "success",
+  },
+  {
+    profile:
+      "https://demos.themeselection.com/marketplace/materio-mui-react-nextjs-admin-template/demo-4/images/avatars/8.png",
+    tag: "Business",
+    date: {
+      month: "Sep",
+      day: 6,
+      from: "12:00",
+      to: "15:30",
+    },
+    title: "Listening old songs",
+    color: "info",
   },
 ];
 
