dadcecb61c4384bec8fba4a82c2075488d036779
Ashik-AD <looge321@gmail.com>
2022-09-24 13:34:30 +0545

    fix intensity color option 🔵⚪🟣

--- src/utils/genColorShades.ts
+++ src/utils/genColorShades.ts
@@ -1,6 +1,6 @@
 const genColorShades = (color: string, option?: { total?: number, intensity?: number, from?: number }): string[] => {
     const colors = Array(option?.total ? option.total : 9).fill(0);
     const colorCodes = color.toString().split(/\D+/).join().split(",").splice(1, 3).join(", ").toString();
-    return colors.map((_, index) => (`rgba(${colorCodes}, 0.${(option?.from ? option.from : 9) - (option?.intensity ? option?.intensity * index + 1 : index)})`))
+    return colors.map((_, index) => (`rgba(${colorCodes}, 0.${option?.intensity ? option.intensity : (option?.from ? option.from : 9) - index})`))
 }
 export default genColorShades
