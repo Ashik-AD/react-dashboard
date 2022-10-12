a3f285a743982e6b8637d278864825ebbbedef2d
Ashik-AD <looge321@gmail.com>
2022-10-12 22:30:57 +0545

    data grid overflow fixed

--- src/components/layout/data-grid/styled.ts
+++ src/components/layout/data-grid/styled.ts
@@ -3,9 +3,9 @@ import { StyledProps } from './type'
 const StyledDataGrid = styled("div") <StyledProps>`
 width: ${({ width }) => width ? width : "100%"};
 padding: 20px 0px;
+overflow-x: auto;
 .data_grid_body {
     height: ${({ height }) => height ? height : "100%"};
-    overflow-y: auto;
 }
 `;
 
