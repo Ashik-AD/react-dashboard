adda2c18bcba9a046ca7a9731e0757f783e2a095
Ashik-AD <looge321@gmail.com>
2022-10-12 22:27:28 +0545

    fix width and reponsiveness issue

--- src/components/layout/grid/styled.ts
+++ src/components/layout/grid/styled.ts
@@ -6,6 +6,11 @@ const GridLayout = styled('div') <GridLayoutProps>`
     display: flex;
     flex-wrap: wrap;
     width: 100%;
+
+    width: calc(100% + ${({ spacing, columnSpacing }) => (spacing || columnSpacing || 0) + "rem"});
+    margin-left: ${({ spacing, columnSpacing }) => (spacing || columnSpacing) ? `-${(spacing || columnSpacing)}rem` : 0};
+    ${({ noShifting }) => noShifting && "margin-left: 0rem; width: 100%;"}
+
     flex-direction: ${({ direction }) => direction === 'column' ? 'column' : direction === 'row-reverse' ? 'row-reverse' : direction === 'column-reverse' ? 'column-reverse' : 'row'};
 
     align-items: ${({ alignItems }) => alignItems === 'flex-start' ? 'flex-start' : alignItems === 'center' ? 'center' : alignItems === 'flex-end' ? 'flex-end' : alignItems === 'stretch' ? 'stretch' : alignItems === 'baseline' ? 'baseline' : 'flex-start'};
@@ -24,65 +29,52 @@ const GridLayout = styled('div') <GridLayoutProps>`
     @media screen and (min-width: 0px){
         & {
             ${({ spacing }) => typeof spacing === 'object' && ` .grid-item {padding-left: ${spacing.xs}rem; padding-bottom: ${spacing.xs}rem;}`
-    }
+        }
             ${({ rowSpacing }) => typeof rowSpacing === 'object' && ` .grid-item {padding-bottom: ${rowSpacing.xs}rem;}`
-    }
+        }
             ${({ columnSpacing }) => typeof columnSpacing === 'object' && ` .grid-item {padding-left: ${columnSpacing.xs}rem;}`
-    }
+        }
 }
     }
 
     @media screen and (min-width: 600px){
-            margin-left: 0;
-            & {
 
+            & {
                 ${({ spacing }) => typeof spacing === 'object' && ` .grid-item {padding-left: ${spacing.sm}rem; padding-bottom: ${spacing.sm}rem;}`
-    }
+        }
                 ${({ rowSpacing }) => typeof rowSpacing === 'object' && ` .grid-item {padding-bottom: ${rowSpacing.sm}rem;}`
-    }
+        }
                 ${({ columnSpacing }) => typeof columnSpacing === 'object' && ` .grid-item {padding-left: ${columnSpacing.sm}rem;}`
-    }
+        }
             }
     }
     @media screen and (min-width: 900px){
-        
             ${({ spacing }) => typeof spacing === 'object' && `& .grid-item {padding-left: ${spacing.md}rem; padding-bottom: ${spacing.md}rem;}`
-    }
+        }
             ${({ rowSpacing }) => typeof rowSpacing === 'object' && `& .grid-item {padding-bottom: ${rowSpacing.md}rem;}`
-    }
+        }
             ${({ columnSpacing }) => typeof columnSpacing === 'object' && `& .grid-item {padding-left: ${columnSpacing.md}rem;}`
-    }
+        }
     }
     @media screen and (min-width: 1200px){
-        ${({ spacing, columnSpacing }) => (spacing || columnSpacing) && `margin-left: -${(spacing || columnSpacing)}rem;`}
-        ${({ noShifting }) => noShifting && "margin-left: 0rem;"}
+
+        // ${({ spacing, columnSpacing }) => (spacing || columnSpacing) && `margin-left: -${(spacing || columnSpacing)}rem;`}
         ${({ spacing }) => typeof spacing === 'object' && `& .grid-item {padding-left: ${spacing.lg}rem; padding-bottom: ${spacing.lg}rem;}`
-    }
+        }
         ${({ rowSpacing }) => typeof rowSpacing === 'object' && `& .grid-item {padding-bottom: ${rowSpacing.lg}rem;}`
-    }
+        }
         ${({ columnSpacing }) => typeof columnSpacing === 'object' && `& .grid-item {padding-left: ${columnSpacing.lg}rem;}`
-    }
+        }
     }
     @media screen and (min-width: 1536px){
         
             ${({ spacing }) => typeof spacing === 'object' && `& .grid-item {padding-left: ${spacing.xlg}rem; padding-bottom: ${spacing.xlg}rem;}`
-    }
+        }
             ${({ rowSpacing }) => typeof rowSpacing === 'object' && `& .grid-item {padding-bottom: ${rowSpacing.xlg}rem;}`
-    }
+        }
             ${({ columnSpacing }) => typeof columnSpacing === 'object' && `& .grid-item {padding-left: ${columnSpacing.xlg}rem;}`
-    }
-    }
-
-    @media screen and (max-width: 600px){
-        
-            margin-left: 0;
-
-            .grid-container {
-                margin-left: 0!important;
-            }
         }
     }
-
 `;
 export const StyledGridInner = styled(GridLayout) <GridLayoutProps>`
     width: ${({ spacing, columnSpacing }) => (spacing || columnSpacing) ? `calc(100% + ${spacing || columnSpacing}rem)` : `100%`};
@@ -91,10 +83,5 @@ export const StyledGridInner = styled(GridLayout) <GridLayoutProps>`
     .grid-item {
         ${({ columnSpacing, spacing }) => !(columnSpacing || spacing) && "padding-left: 0;"}
     }
-    @media screen and (max-width: 600px){
-        margin-left: 0rem;
-    }
-
-
 `
 export default GridLayout;
