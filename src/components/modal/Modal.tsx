421a95e4fb22b78ffe936d4402a42962fea55ed3
Ashik-AD <looge321@gmail.com>
2022-11-12 13:56:22 +0545

    add modal

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const rootModal = document.getElementById("root-modal") as HTMLDivElement;
+
+interface Props {
+  children: ReactNode;
+  handleOutClick?: (event: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+const Modal = ({ children, handleOutClick }: Props) => {
+  const modalContentWrp = useRef(document.createElement("div"));
+  const modalOverlay = useRef(document.createElement("div"));
+  useEffect(() => {
+    const current = modalContentWrp.current;
+    current.classList.add("modal-content-wrapper");
+    modalOverlay.current.classList.add("modal-overlay");
+
+    if (handleOutClick) {
+      modalOverlay.current.addEventListener("click", handleOutClick);
+    }
+    current.appendChild(modalOverlay.current);
+    rootModal.append(current);
+
+    return () => void rootModal.removeChild(current);
+  }, []);
+  return createPortal(children, modalContentWrp.current);
+};
+export default Modal;
