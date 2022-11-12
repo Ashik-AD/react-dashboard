20f1293427fb579d3a9668ed418e1a34653918e8
Ashik-AD <looge321@gmail.com>
2022-11-12 14:04:10 +0545

    add login history and project list

--- src/api/server.ts
+++ src/api/server.ts
@@ -3,26 +3,30 @@ import chats from '../api/chats.json'
 import chatContact from "./chatContact.json";
 import invocices from './invoice.json';
 import users from './users.json'
+import userProject from './projects.json';
+import loggedDevice from './loginHistory.json'
 export default function () {
     return createServer({
         routes() {
             this.get('/api/test/', () => ({
                 data: [{ id: 2, text: "hola" }]
-            })),
-                this.get('api/chat/userprofile', () => ({
-                    user_profile: {
-                        uid: 728232,
-                        user_name: "John Doe",
-                        avatar_src: "/src/image/humans/3.png",
-                        role: "admin",
-                        status: "Online",
-                        about: "🥰 I have always thought the actions of men the best interpreters of their thought.✌️"
-                    }
-                })),
-                this.get('/api/chat/chats', () => chats)
+            }))
+            this.get('api/chat/userprofile', () => ({
+                user_profile: {
+                    uid: 728232,
+                    user_name: "John Doe",
+                    avatar_src: "/src/image/humans/3.png",
+                    role: "admin",
+                    status: "Online",
+                    about: "🥰 I have always thought the actions of men the best interpreters of their thought.✌️"
+                }
+            }))
+            this.get('/api/chat/chats', () => chats)
             this.get('/api/chat/contacts', () => chatContact)
             this.get('/api/invoices', () => invocices)
             this.get('/api/user/users', () => users)
+            this.get('/api/user/projects', () => userProject)
+            this.get('/api/user/logged-recent', () => loggedDevice)
         }
     })
 }
