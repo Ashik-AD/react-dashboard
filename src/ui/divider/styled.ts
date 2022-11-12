dd6916995ee695cc43a17bc294ee80af2dbed114
Ashik-AD <looge321@gmail.com>
2022-11-12 13:30:17 +0545

    adjust width and margin

--- src/ui/divider/styled.ts
+++ src/ui/divider/styled.ts
@@ -2,10 +2,13 @@ import styled from 'styled-components'
 import DividerProps from './type';
 
 const Div = styled("div") <DividerProps>`
+  position: relative;
   height: 0.5px;
+  width: 100%;
   ${({ vertical }) => vertical && `width: 1px; 
   min-height: 0px; 
   height: 100%;`}
   background: ${({ theme }) => theme.mode.name === "dark" ? "rgb(255 255 255 / 12%)" : "rgb(0 0 0 / 12%)"};
+  margin: 0px auto;
 `;
 export default Div
