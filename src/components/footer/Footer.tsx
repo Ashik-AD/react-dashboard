f6ad9319d6476193b99376b1f431b8ce32d5d4b9
Ashik-AD <looge321@gmail.com>
2023-01-03 13:10:34 +0545

    add appfooter position

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,31 @@
 import { Icon } from "@iconify/react";
 import styled from "styled-components";
+import useTheme from "../../theme/useTheme";
 import { Text } from "../../ui";
 import Box from "../box/Box";
 
 const Footer = () => {
+  const {
+    theme: {
+      layout: { footerPosition },
+    },
+  } = useTheme();
+  if (footerPosition === "hidden") return <></>;
+
   return (
-    <StyledFooter>
+    <StyledFooter
+      className={`footer_position--${footerPosition} ${
+        footerPosition === "fixed" ? "fg-theme" : ""
+      }`}
+    >
       <Text varient="body1" weight="medium">
         Made with ❤️ by{" "}
-        <a href="https://github.com/Ashik-AD" target={"_blank"}>
-          AshikDhimal
+        <a
+          href="https://github.com/Ashik-AD"
+          target={"_blank"}
+          className="text-primary"
+        >
+          Ashik Dhimal
         </a>
       </Text>
       <Box display="flex" space={0.6}>
@@ -30,4 +46,13 @@ const StyledFooter = styled("footer")`
   align-item: center;
   justify-content: space-between;
   padding: 1rem 1.2rem;
+  z-index: 1;
+
+  &.footer_position--fixed {
+    position: sticky;
+    bottom: 0;
+    border-radius: 0.6rem;
+    margin: 0 auto;
+    width: calc(100% - 2rem);
+  }
 `;
