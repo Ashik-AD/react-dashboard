6052e42d5eed7034dba82d610338972c2f0ef4c2
Ashik-AD <looge321@gmail.com>
2023-01-06 17:45:12 +0545

    change .env api url variable from ..._API_ENDPOINT to ..._API_URL

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 
 const mode = import.meta.env.DEV ? "dev" : "prod";
-const rootPath = mode === "dev" ? import.meta.env.VITE_DEV_API_ENDPOINT : import.meta.env.VITE_PROD_API_ENDPOINT;
+const rootPath = mode === "dev" ? import.meta.env.VITE_DEV_API_URL : import.meta.env.VITE_PROD_API_URL;
 
 export async function client(endpoint: string, { body, ...customConfig } :any = {}) {
     const headers = { 'Content-Type': 'application/json' }
