f03d4ab068a5267608dc0d2e72ff6dd9e74ec9c5
Ashik-AD <looge321@gmail.com>
2023-01-09 17:03:35 +0545

    add api url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 
 const isAppInProd = import.meta.env.PROD;
-const rootPath = !isAppInProd ? import.meta.env.VITE_DEV_API_URL : import.meta.env.VITE_PROD_API_URL;
+const rootPath = !isAppInProd ? "/api" : "https://react-dashboard-server-production.up.railway.app";
 
 export async function client(endpoint: string, { body, ...customConfig } :any = {}) {
     const headers = { 'Content-Type': 'application/json' }
