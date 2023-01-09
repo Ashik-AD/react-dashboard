1b5cd313fc4fdfdd4b5c7d6705b0625bfc6650dd
Ashik-AD <looge321@gmail.com>
2023-01-09 13:16:08 +0545

    change variable mode to isAppInProd

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,20 +1,20 @@
 
-const mode = import.meta.env.DEV ? "dev" : "prod";
-const rootPath = mode === "dev" ? import.meta.env.VITE_DEV_API_URL : import.meta.env.VITE_PROD_API_URL;
+const isAppInProd = import.meta.env.PROD;
+const rootPath = !isAppInProd ? import.meta.env.VITE_DEV_API_URL : import.meta.env.VITE_PROD_API_URL;
 
 export async function client(endpoint: string, { body, ...customConfig } :any = {}) {
     const headers = { 'Content-Type': 'application/json' }
     const paths = endpoint.replaceAll("/", " ").trim().split(" ");
     // const modeProdJsonPath = paths.length >= 2 ? paths[paths.length - 1]: ""
-    const modeProdJsonPath = mode === "prod" && paths.length >= 2 ? paths[paths.length - 1]: ""
+    const modeProdJsonPath = isAppInProd && paths.length >= 2 ? paths[paths.length - 1]: ""
     const config = {
         method: body ? "POST" : "GET",
         ...customConfig,
         headers: {
             ...headers,
             ...customConfig.headers,
-            "X-MASTER-KEY": mode === "prod" ? `$2b$10$${import.meta.env.VITE_API_KEY}` : "",
-            "X-JSON-path": mode === "prod" && modeProdJsonPath ? `$.${modeProdJsonPath}` : "",
+            "X-MASTER-KEY": isAppInProd ? `$2b$10$${import.meta.env.VITE_API_KEY}` : "",
+            "X-JSON-path": isAppInProd && modeProdJsonPath ? `$.${modeProdJsonPath}` : "",
             "Access-Control-Allow-Origin": "*"
         }
     }
@@ -25,10 +25,10 @@ export async function client(endpoint: string, { body, ...customConfig } :any = 
 
     let data;
     try {
-        const response = await window.fetch(`${rootPath}${mode === "dev" ? endpoint : `/${paths[0]}/`}`, config);
+        const response = await window.fetch(`${rootPath}${!isAppInProd ? endpoint : `/${paths[0]}/`}`, config);
         const data = await response.json();
         if (response.ok) {
-             return mode === "dev"  ? data : Array.isArray(data.record) ?  data.record?.length > 1 ? data.record : data.record[0] : data.record;
+             return !isAppInProd  ? data : Array.isArray(data.record) ?  data.record?.length > 1 ? data.record : data.record[0] : data.record;
         }
         throw new Error(response.statusText)
     }
